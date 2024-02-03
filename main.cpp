44af164490a6a70e20ff0b0803350b971e644615
Youssef Mohamed <youssef.mohamed.47048@gmail.com>
2024-02-03 07:00:13 +0200

    apduting the project and use unorder_map to enhance the performance

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-map<int,deque<string> >m;
-void add(){
+unordered_map<int,deque<string> >m;
+void add()
+{
     cout<<"Enter specialization, name, status:  ";
     bool status;
     int num;
@@ -14,24 +15,32 @@ void add(){
         m[num].push_back(name);
 
 }
-void print(){
+void print()
+{
     unordered_map<int,deque<string> >::iterator itr;
-    for(int i = 1 ;i<=20;i++){
-        if((m[i].size())){
+    for(int i = 1 ; i<=20; i++)
+    {
+        if((m[i].size()))
+        {
             cout<<"There are "<<m[i].size()<<" patients in specialization "<<i<<endl;
-            for(int j = 0 ; j < m[i].size();j++ ){
+            for(int j = 0 ; j < m[i].size(); j++ )
+            {
                 cout<<m[i][j]<<" \n";
             }
         }
     }
 }
-void get_next(){
+void get_next()
+{
     int num ;
     cout<<"Enter specialization \n";
     cin>>num;
-    if(m[num].empty()){
+    if(m[num].empty())
+    {
         cout<<"No patients at the moment, Have rest, Dr\n";
-    }else {
+    }
+    else
+    {
         cout<<m[num].front()<<" go with the doctor\n";
         m[num].pop_front();
     }
@@ -41,38 +50,41 @@ int main()
     string st;
     int n;
 
-    do{
-       cout<< "Enter your choice\n";
-       cout<< "1) Add new patient\n";
-       cout<<"2) Print all patients\n";
-       cout<<"3) Get next patient\n";
+    do
+    {
+        cout<< "Enter your choice\n";
+        cout<< "1) Add new patient\n";
+        cout<<"2) Print all patients\n";
+        cout<<"3) Get next patient\n";
         cout<<"4) Exit\n";
         cin >>st;
-        if(isdigit(st[0])){
-         n = stoi(st);
-        switch(n){
-        case 1:
-            add();
-            break;
-        case 2:
-           print();
-            break;
-        case 3:
-            get_next();
-            break;
-         case 4:
-             return 0;
-        default:
-            cout<<"unvalid number\n";
-
-
-
+        if(isdigit(st[0]))
+        {
+            n = stoi(st);
+            switch(n)
+            {
+            case 1:
+                add();
+                break;
+            case 2:
+                print();
+                break;
+            case 3:
+                get_next();
+                break;
+            case 4:
+                return 0;
+            default:
+                cout<<"unvalid number\n";
+            }
         }
-        }else{
+        else
+        {
             cout<<"unvalid number\n";
             n = 1;
         }
 
-    }while(n);
+    }
+    while(n);
     return 0;
 }
